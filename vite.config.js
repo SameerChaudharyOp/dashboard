5ceaee3f832bb5a283edc72313815bfb85e17c53
SameerChaudharyOp <sameerchaudhary749@gmail.com>
2025-01-13 09:02:31 +0530

    #1

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,12 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      // Mark 'recharts' as an external dependency so that it's not bundled
-      external: ["recharts"],
+      // Externalize 'recharts' so it is not bundled with the final output
+      external: ['recharts'],
     },
   },
-});
+})
