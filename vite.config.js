b920b2ea29890c61b67c90ec65c509e02037c9e3
SameerChaudharyOp <sameerchaudhary749@gmail.com>
2025-01-13 09:05:40 +0530

    #2

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      // Externalize 'recharts' so it is not bundled with the final output
-      external: ['recharts'],
+      external: ['recharts', 'papaparse'],
     },
   },
 })
